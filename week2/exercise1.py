5ea292272fd119dcd936a7af46aa5e1f0ae9ef33
Eddie Azzi <Eddie.Azzi@outlook.com>
2019-06-14 11:31:51 +1000

    Week2

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -10,20 +10,25 @@ import platform
 
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
-
+#A list of words is used to show what "some_words" is equal to. e.g. an example of "some words" would be "does" and "line".
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+#This will print a single word in the list above.
 for word in some_words:
     print(word)
 
+#This will print a number (labelled as "x") of words from the list above.
 for x in some_words:
     print(x)
 
+#This will print every word in the list above. Possibly creating the sentence, "what does this line do?". OR this will simply print "some_words", which equals to the list.
 print(some_words)
 
+#The amount of elements in the list is greater than 3. If a word contains more than 3 words, it will print the word.
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
 
+#The useful function is defined by the returned tuple. This is then printed.
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -32,4 +37,5 @@ def usefulFunction():
     """
     print(platform.uname())
 
+#The useful function 
 usefulFunction()
