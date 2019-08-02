0657aa8da1fbe01f677c8f89021386fb12848e68
Eddie Azzi <Eddie.Azzi@outlook.com>
2019-08-02 11:36:30 +1000

    up to pet filter

---
+++ week8/not_an_exam.py
@@ -0,0 +1,161 @@
+# -*- coding: UTF-8 -*-
+"""
+I'm in UR exam.
+This is the same as the weekly exercises, fill in the functions,
+and test them to see if they work.
+You have 2 hours.
+
+You need to copy this file to your me/week8 folder
+You need to rename it to exercise1.py
+"""
+import string
+import time
+
+
+def greet(name="Towering Timmy"):
+   return ("Hello" + " " + name)
+
+def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
+    number_of_threes = 0 
+    for i in input_list:
+        if i == 3:
+            number_of_threes += 1
+    
+    return number_of_threes
+
+def fizz_buzz():
+    fizzBuzzList = []
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzBuzzList.append("FizzBuzz")
+        elif i % 3 == 0:
+            fizzBuzzList.append("Fizz")
+        elif i % 5 == 0:
+            fizzBuzzList.append("Buzz")
+        else:
+            fizzBuzzList.append(i)
+    
+    return  fizzBuzzList
+
+
+
+def put_behind_bars(input_string="very naughty boy"):
+    output_string = "|"
+    for c in input_string:
+        output_string = output_string + c + "|"
+    return output_string
+
+
+def pet_filter(letter="a"):
+    # fmt: off
+    pets = [
+            "dog", "goat","pig","sheep","cattle","zebu","cat","chicken",
+            "guinea pig","donkey","duck","water buffalo","western honey bee",
+            "dromedary camel","horse","silkmoth","pigeon","goose","yak",
+            "bactrian camel","llama","alpaca","guineafowl","ferret",
+            "muscovy duck","barbary dove","bali cattle","gayal","turkey",
+            "goldfish","rabbit","koi","canary","society finch","fancy mouse",
+            "siamese fighting fish","fancy rat and lab rat","mink","red fox",
+            "hedgehog","guppy",]
+    # fmt: on
+    new_list = []
+    for word in pets:
+        if str(letter) in word:
+            new_list.append(word)
+
+    return new_list
+    #return []
+
+
+def best_letter_for_pets():
+    """Return the letter that is present at least once in the most pet names.
+    Reusing the pet_filter, find the letter that gives the longest list of pets
+    TIP: return just a letter, not the list of animals.
+    """
+    import string
+
+    the_alphabet = string.ascii_lowercase
+
+    return ""
+
+
+def make_filler_text_dictionary():
+    """Make a dictionary of random words filler text.
+    There is a random word generator here:
+    https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=4 
+    If we set minLength=18 and maxLength=18, we will get something like this:
+    >>> url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=18"
+    >>> r = requests.get(url)
+    >>> r.text # will get you a string, something like this:
+    >>> "occipitosphenoidal"
+    
+    Return a dictionary where the keys are numbers, and the values are lists of
+    words. e.g.
+    { 
+        3: ['Sep', 'the', 'yob'],
+        4: ['aaaa', 'bbbb', 'cccc'],
+        ...
+        7: ['aaaaaaa', 'bbbbbbb', 'ccccccc']
+    }
+    Use the API to get the 3 words.
+    
+    The dictionary should have the numbers between 3 and 7 inclusive.
+    (i.e. 3, 4, 5, 6, 7 and 3 words for each)
+    TIP: you'll need the requests library
+    """
+
+    import requests
+
+    return {}
+
+
+def random_filler_text(number_of_words=200):
+    """Make a paragraph of random filler text.
+    Using the dictionary returned by make_filler_text_dictionary, make a
+    paragraph of text using randomly picked words. Each word should be a random
+    length, and a random one of the 3 words.
+    Make the paragraph have number_of_words words in it.
+    Return it as a string
+    TIP: you'll need the random library, 
+        see line 77 of week4/hangman_leadboard.py for an example.
+    """
+    import random
+
+    return ""
+
+
+def fast_filler(number_of_words=200):
+    """Reimplement random_filler_text.
+    This time, the first time the code runs, save the dictionary returned
+    from make_filler_text_dictionary to a file.
+    On the second run, if the file already exists use it instead of going to
+    the internet.
+    Use the filename "dict_racey.json"
+    TIP: you'll need the os and json libraries
+    TIP: you'll probably want to use json dumps and loads to get the dictionary
+    into and out of the file. Be careful when you read it back in, it'll
+    convert integer keys to strings.
+
+    If you get this one to work, you are a Very Good Programmer™!
+    """
+
+    return paragraph
+
+
+if __name__ == "__main__":
+    print("greet:", greet())
+    print("three_counter:", three_counter())
+    print("fizz_buzz:", fizz_buzz())
+    print("put_behind_bars:", put_behind_bars())
+    print("pet_filter:", pet_filter())
+    print("best_letter_for_pets:", best_letter_for_pets())
+    print("make_filler_text_dictionary:", make_filler_text_dictionary())
+    print("random_filler_text:", random_filler_text())
+    print("fast_filler:", fast_filler())
+    for i in range(10):
+        print(i, fast_filler())
+    print(
+        "These are mini tests, they show you some output.",
+        "\nDon't forget to run the real tests.",
+        "\nThey test your code against the non-default arguments",
+    )
